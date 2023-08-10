d455bad80de89e35160e9815037e9151a535d920
Joseph <josephomoregie1@outlook.com>
2023-08-10 07:40:53 +0100

    created seperate files for bill and imported them to main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-class Bill:
-    def __init__(self,amount,period):
-        self.amount = amount
-        self.period = period
+import Bill
+import Flatmate
+import PdfReport
 
-class Flatmate:
-
-    def __init__(self, name,    days_in_house):
-        self.name = name
-        self.days_in_house = days_in_house
-
-    def pays(self, bill):
-        pass
-
-class PdfReport:
-
-    def __int__(self,filename):
-        self.filename = filename
-
-    def generate(self,flatmate1,flatmate2,bill):
-        pass
+the_bill = Bill.Bill(120, "March 2021")
+mary = Flatmate.Flatmate("Mary", 25)
+john = Flatmate.Flatmate("John", 20)
