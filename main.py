54e747e34a0ff66dec38ddc0178b39944579b5b6
Joseph <josephomoregie1@outlook.com>
2023-08-10 08:17:00 +0100

    added interface code to main.py and implemented pays function in Bill

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import Bill
 import Flatmate
 import PdfReport
 
-the_bill = Bill.Bill(120, "March 2021")
-mary = Flatmate.Flatmate("Mary", 25)
-john = Flatmate.Flatmate("John", 20)
+bill_amount = int(input("Hey user, enter the bill amount: "))
+period = input("What is the bill period? E.g December 2020: ")
+the_bill = Bill.Bill(bill_amount, period)
+
+user = input("What is your name? ")
+user_duration = input(f"How many days did {user} stay in the house during the bill period? ")
+users_flatmate_name = input("What is the name of the other flatmate? ")
+flatmate_stay = input(f"How many days did {users_flatmate_name} stay in the house during the bill period? ")
+
+flatmate_1 = Flatmate.Flatmate(user, user_duration)
+flatmate_2 = Flatmate.Flatmate(users_flatmate_name, flatmate_stay)
+
+print(f"{user} pays: {flatmate_1.pays(the_bill)}")
+print(f"{users_flatmate_name} pays: {flatmate_2.pays(the_bill)}")
